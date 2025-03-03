53dfbe94cbe27a623b092a56dbd8961074304348
Dipesh Adhikari <dipeshadh24@gmail.com>
2025-03-03 16:14:00 +0545

    created middleware

---
+++ app/middleware/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { createSupabaseReqResClient } from "@/lib/supabase/server-client";
+
+export default async function middleware(request: NextRequest) {
+  const response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createSupabaseReqResClient(request, response);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const user = session?.user;
+
+  if (!user && request.nextUrl.pathname.startsWith("/account")) {
+    return NextResponse.redirect("/");
+  }
+  return response;
+}
+
+export const config = {
+  matcher: ["/"],
+};
---
+++ lib/supabase/use-session.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { createSupabaseBrowserClient } from "./browser-client";
+import { Session } from "@supabase/supabase-js";
+
+export default function useSession() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    const supabase = createSupabaseBrowserClient();
+
+    const getSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      setSession(session);
+    };
+
+    getSession();
+  }, []);
+
+  return session;
+}
